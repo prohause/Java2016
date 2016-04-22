dad8f8ab38674ed94e12cc2d23ddf560efb79acd
prohause <prohause@abv.bg>
2016-04-22 17:52:51 +0300

    22.04.2016

--- Exams/JavaFundamentalsExam28February2016/src/Problem1CollectResources.java
+++ Exams/JavaFundamentalsExam28February2016/src/Problem1CollectResources.java
@@ -6,5 +6,25 @@ import java.util.Scanner;
 public class Problem1CollectResources {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(" ");
+        int nPaths = scanner.nextInt();
+        scanner.nextLine();
+        int[][] path = new int[nPaths][2];
+
+        for (int i = 0; i < path.length; i++) {
+            String[] current = scanner.nextLine().split(" ");
+            path[i][0] = Integer.parseInt(current[0]);
+            path[i][1] = Integer.parseInt(current[1]);
+        }
+
+        for (String s : input) {
+            System.out.println(s);
+        }
+
+        for (int[] ints : path) {
+            for (int anInt : ints) {
+                System.out.println(anInt);
+            }
+        }
     }
 }
